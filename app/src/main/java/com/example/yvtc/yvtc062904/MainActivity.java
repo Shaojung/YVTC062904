fd3c9ef450b3b0bb9a53d3e53ba9875a31d151cd
Shaojung <shaojung@gmail.com>
2017-06-29 16:21:56 +0800

    use SimpleAdapter to fit spinner data.

--- app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
@@ -6,16 +6,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner spinner, spinner2;
+    Spinner spinner, spinner2, spinner3;
     TextView tv, tv2;
     ArrayList<String> mylist;
+    ArrayList<HashMap<String, String>> list2;
     EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         tv2 = (TextView) findViewById(R.id.textView2);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
         ed = (EditText) findViewById(R.id.editText);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -51,6 +55,25 @@ public class MainActivity extends AppCompatActivity {
         mylist.add("芭樂");
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mylist);
         spinner2.setAdapter(adapter);
+
+        list2 = new ArrayList<>();
+        HashMap<String, String> m1 = new HashMap<>();
+        m1.put("city", "台北");
+        m1.put("code", "02");
+        HashMap<String, String> m2 = new HashMap<>();
+        m2.put("city", "台中");
+        m2.put("code", "04");
+        HashMap<String, String> m3 = new HashMap<>();
+        m3.put("city", "高雄");
+        m3.put("code", "07");
+
+        list2.add(m1);
+        list2.add(m2);
+        list2.add(m3);
+
+        SimpleAdapter adapter2 = new SimpleAdapter(MainActivity.this, list2, android.R.layout.simple_list_item_2,
+                                    new String[] {"city", "code"}, new int[] {android.R.id.text1, android.R.id.text2});
+        spinner3.setAdapter(adapter2);
 
 
     }
