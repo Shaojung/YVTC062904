77934478ba193561187b9c94ba805da12acaa283
Shaojung <shaojung@gmail.com>
2017-06-29 15:42:43 +0800

    use ArrayAdapter to fit spinner data.

--- app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner spinner;
+    Spinner spinner, spinner2;
     TextView tv, tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textView);
         tv2 = (TextView) findViewById(R.id.textView2);
         spinner = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -35,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        String[] arr2 = {"蘋果", "香蕉", "鳳梨", "芭樂"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, arr2);
+        spinner2.setAdapter(adapter);
+
+
     }
     public void click1(View v)
     {
