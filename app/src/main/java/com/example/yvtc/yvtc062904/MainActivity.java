ea9bae5e3724e65374a568f63129e0c6632b039e
Shaojung <shaojung@gmail.com>
2017-06-29 15:58:03 +0800

    use ArrayAdapter to fit spinner data.

--- app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc062904/MainActivity.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Spinner spinner, spinner2;
     TextView tv, tv2;
+    ArrayList<String> mylist;
+    EditText ed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tv2 = (TextView) findViewById(R.id.textView2);
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ed = (EditText) findViewById(R.id.editText);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -38,11 +44,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        String[] arr2 = {"蘋果", "香蕉", "鳳梨", "芭樂"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, arr2);
+        mylist = new ArrayList<>();
+        mylist.add("蘋果");
+        mylist.add("香蕉");
+        mylist.add("鳳梨");
+        mylist.add("芭樂");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, mylist);
         spinner2.setAdapter(adapter);
 
 
+    }
+
+    public void clickAdd(View v)
+    {
+        mylist.add(ed.getText().toString());
     }
     public void click1(View v)
     {
